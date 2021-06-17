d8ff539c39b8544850691b814a68197c2dbac3a9
= <tor@hearkitty.com>
2021-06-17 13:45:48 -0600

    refined form and added some notes to us

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-form = $('form')
 giftYes = $('#gift-yes')
 giftRecipientDiv = $('#gift-recipient-div')
+giftRecipientDiv.hide()
+
+
+giftYes.on("click", function(e) {
+    if (document.querySelector('#gift-yes').checked) {
+        console.log('show it')
+        giftRecipientDiv.show()
+    } else {
+        console.log('hide it')
+        giftRecipientDiv.hide()
+    };
+})
+
+$('form').on("submit", function(e) {
+    e.preventDefault()
+    // email info to us with confirmation email to them
+})
