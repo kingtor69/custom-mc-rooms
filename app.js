34b7176b6958ae07ade8e3b465fdaee3b0041066
Tor Kingdon <tor@hearkitty.com>
2021-07-24 11:42:53 -0600

    confirmation page info is populated

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+for (let field of $('form')) {
+    console.log(field)
+}
 
 $('form').on("submit", (e) => {
     for (let field of e.target) {
         if (field.id !== "show-color") {
-            sessionStorage.setItem(field.id, field.value)
+            sessionStorage.setItem(field.id, field.value);
         };
     };
 });
 
 $('#color').on("change", (e) => {
-    $('#show-color').removeClass()
-    $('#show-color').addClass(`${e.target.value} btn btn-block`)
+    $('#show-color').removeClass();
+    $('#show-color').addClass(`${e.target.value} btn btn-block`);
+});
+
+for (let key in sessionStorage) {
+    $(`#data-${key}`).text(sessionStorage[key]);
+};
+
+$('#clear-form').on('click', () => {
+    sessionStorage.clear();
+    location.reload();
 });
