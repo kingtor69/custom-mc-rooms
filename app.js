4fc942b1f54df165317c353b0825acb6a968a278
Tor Kingdon <tor@hearkitty.com>
2021-07-31 14:27:42 -0600

    tweaks with Aspen

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ $('form').on("submit", (e) => {
 });
 
 $('#color').on("change", (e) => {
+    $('#color').removeClass();
+    if (e.target.value) {
+        $('#color').addClass(`form-control ${e.target.value}`);
+    } else {
+        $('#color').addClass(`form-control text-muted`)
+    }
     $('#show-color').removeClass();
     $('#show-color').addClass(`${e.target.value} btn btn-block`);
 });
