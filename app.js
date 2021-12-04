880bba1287039cd70ad9dd55095432221d9870da
Tor Kingdon <tor@hearkitty.com>
2021-12-04 12:53:13 -0700

    updating sessionStorage for payment page

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-for (let field of $('form')) {
-    console.log(field)
-}
-
 $('form').on("submit", (e) => {
     for (let field of e.target) {
-        if (field.id !== "show-color") {
+        if (field.class === "field-store") {
             sessionStorage.setItem(field.id, field.value);
         };
     };
@@ -19,6 +15,14 @@ $('#color').on("change", (e) => {
     }
     $('#show-color').removeClass();
     $('#show-color').addClass(`${e.target.value} btn btn-block`);
+});
+
+$('.template-inputs').on("change", (e) => {
+    sessionStorage.setItem('template', e.currentTarget.id);
+});
+
+$('.animal-inputs').on("change", (e) => {
+    sessionStorage.setItem('animal', e.currentTarget.id);
 });
 
 for (let key in sessionStorage) {
